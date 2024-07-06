0ca3965c81a84740bd9454c5558c43642b5937cd
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2024-07-07 00:14:16 +0900

    ✨ versionを読めるように

--- libs/aiotraq/aiotraq/__init__.py
+++ libs/aiotraq/aiotraq/__init__.py
@@ -1,7 +1,9 @@
 """A client library for accessing traQ v3"""
 
 from .client import AuthenticatedClient, Client
+from importlib.metadata import version
 
+__version__ = version(__package__)
 __all__ = (
     "AuthenticatedClient",
     "Client",
--- libs/message/aiotraq_message/__init__.py
+++ libs/message/aiotraq_message/__init__.py
@@ -1,4 +1,6 @@
 from .manager import TraqMessageManager
 from .message import TraqMessage
+from importlib.metadata import version
 
+__version__ = version(__package__)
 __all__ = ["TraqMessageManager", "TraqMessage"]
